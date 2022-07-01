56742d944ac139fa9e4f5c9d9d69badce400f341
Norbert Schneider <norbert.schneider@code-intelligence.com>
2022-07-01 10:41:56 +0200

    add tests for instrumented statements

---
+++ src/native.ts
@@ -0,0 +1,11 @@
+
+// TODO: Pass request for next counter to native plugin
+let counter = 0;
+export function nextCounter(): number {
+    return counter++;
+}
+
+// TODO: incrementCounter applies the never-zero policy
+// noinspection JSUnusedLocalSymbols
+export function incrementCounter(id: number) {
+}
