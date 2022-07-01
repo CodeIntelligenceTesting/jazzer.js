942bb248549fc7ce31caee2f7935ba298dcf0785
Norbert Schneider <norbert.schneider@code-intelligence.com>
2022-07-01 10:50:58 +0200

    add prettier and husky

--- src/native.ts
+++ src/native.ts
@@ -1,11 +1,9 @@
-
 // TODO: Pass request for next counter to native plugin
 let counter = 0;
 export function nextCounter(): number {
-    return counter++;
+  return counter++;
 }
 
 // TODO: incrementCounter applies the never-zero policy
 // noinspection JSUnusedLocalSymbols
-export function incrementCounter(id: number) {
-}
+export function incrementCounter(id: number) {}
