2f969ed1dff80df89471eed34f10cea93c98998c
Khaled Yakdan <yakdan@code-intelligence.com>
2022-07-05 14:02:19 +0200

    add a workflow for testing

--- fuzzer/test_fuzzer.js
+++ fuzzer/test_fuzzer.js
@@ -1,10 +1,11 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 const fuzzer = require("./fuzzer");
 
 fuzzer.printVersion();
 
 // Our fake fuzz target.
 const fuzz = (data) => {
-  console.log("Fuzz target called with", data);
-}
+	console.log("Fuzz target called with", data);
+};
 
 fuzzer.startFuzzing(fuzz, ["-runs=0"]);
