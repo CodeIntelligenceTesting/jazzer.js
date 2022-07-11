9f7b48f793909eb4087ec39709c901b7d1c0209e
Norbert Schneider <norbert.schneider@code-intelligence.com>
2022-07-11 15:06:18 +0200

    Build fuzzer package with TypeScript
    This prepares a switch from the fuzzer package to TypeScript to
    provide types of the native plugin to other packages.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,12 +4,11 @@ set -e
 echo "Running jest tests"
 npm run test
 
-echo "Building the native fuzzer addon"
-#npm run compile --workspace=packages/fuzzer
-cd packages/fuzzer
-npm install
+echo "Transpile TypeScript files"
 npm run compile
 
+echo "Building the native fuzzer addon"
+npm run compile --workspace=packages/fuzzer
+
 echo "Running addon tests"
-#npm run test --workspace=packages/fuzzer
-npm run test
+npm run test --workspace=packages/fuzzer
