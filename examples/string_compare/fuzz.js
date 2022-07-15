c83adde46bd367a8f544244705b19e84304b82b7
Khaled Yakdan <yakdan@code-intelligence.com>
2022-07-15 15:44:30 +0200

    split string_compare example into two files

--- examples/string_compare/fuzz.js
+++ examples/string_compare/fuzz.js
@@ -1,15 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const lib = require("./lib");
+
 function fuzz(data) {
-	const s = data.toString();
-	if (s.length !== 16) {
-		return;
-	}
-	if (
-		s.slice(0, 8) === "Awesome " &&
-		s.slice(8, 15) === "Fuzzing" &&
-		s[15] === "!"
-	) {
-		throw Error("Welcome to Awesome Fuzzing!");
-	}
+	lib.fuzzMe(data);
 }
 
-module.exports.fuzz = fuzz;
+exports.fuzz = fuzz;
