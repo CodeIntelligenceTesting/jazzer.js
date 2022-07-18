d799e26f4259b749fa9507a3f4ab382829d82257
Khaled Yakdan <yakdan@code-intelligence.com>
2022-07-18 08:51:56 +0200

    update copyright statement

--- packages/fuzzer/shared/callbacks.cpp
+++ packages/fuzzer/shared/callbacks.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Code Intelligence GmbH
+// Copyright 2022 Code Intelligence GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
