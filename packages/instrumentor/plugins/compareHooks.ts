00c6c372c468803b04059bb74b907223e87acc0d
Khaled Yakdan <yakdan@code-intelligence.com>
2022-07-27 14:59:31 +0200

    Instrument number comparisons

--- packages/instrumentor/plugins/compareHooks.ts
+++ packages/instrumentor/plugins/compareHooks.ts
@@ -22,39 +22,57 @@ export function compareHooks(): PluginTarget {
 	return {
 		visitor: {
 			BinaryExpression(path: NodePath<BinaryExpression>) {
-				// One operand has to be a string literal but not both
-				if (
-					(path.node.left.type !== "StringLiteral" &&
-						path.node.right.type !== "StringLiteral") ||
-					(path.node.left.type === "StringLiteral" &&
-						path.node.right.type === "StringLiteral")
-				) {
-					return;
-				}
-
 				// TODO: Investigate this type, it can not be passed to the call expression
 				if (path.node.left.type == "PrivateName") {
 					return;
 				}
 
-				// Only support equals and not equals operators, the other ones can
-				// not be forwarded to libFuzzer
-				if (
-					path.node.operator === "==" ||
-					path.node.operator === "===" ||
-					path.node.operator === "!=" ||
-					path.node.operator === "!=="
-				) {
-					path.replaceWith(
-						types.callExpression(types.identifier("Fuzzer.traceStrCmp"), [
-							path.node.left,
-							path.node.right,
-							types.stringLiteral(path.node.operator),
-							fakePC(),
-						])
-					);
+				let hookFunctionName: string;
+				if (isStringCompare(path.node)) {
+					hookFunctionName = "Fuzzer.traceStrCmp";
+				} else if (isNumberCompare(path.node)) {
+					hookFunctionName = "Fuzzer.traceNumberCmp";
+				} else {
+					return;
 				}
+
+				path.replaceWith(
+					types.callExpression(types.identifier(hookFunctionName), [
+						path.node.left,
+						path.node.right,
+						types.stringLiteral(path.node.operator),
+						fakePC(),
+					])
+				);
 			},
 		},
 	};
+}
+
+function isStringCompare(exp: BinaryExpression): boolean {
+	// One operand has to be a string literal but not both
+	if (
+		(exp.left.type !== "StringLiteral" && exp.right.type !== "StringLiteral") ||
+		(exp.left.type === "StringLiteral" && exp.right.type === "StringLiteral")
+	) {
+		return false;
+	}
+
+	// Only support equals and not equals operators, the other ones can
+	// not be forwarded to libFuzzer
+	return ["==", "===", "!=", "!=="].includes(exp.operator);
+}
+
+function isNumberCompare(exp: BinaryExpression): boolean {
+	// One operand has to be a string literal but not both
+	if (
+		(exp.left.type !== "NumericLiteral" &&
+			exp.right.type !== "NumericLiteral") ||
+		(exp.left.type === "NumericLiteral" && exp.right.type === "NumericLiteral")
+	) {
+		return false;
+	}
+	return ["==", "===", "!=", "!==", ">", ">=", "<", "<="].includes(
+		exp.operator
+	);
 }
