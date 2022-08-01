04ff0a5bf816fdb32c490c32f3436d4486424ae0
Khaled Yakdan <yakdan@code-intelligence.com>
2022-08-01 09:38:02 +0200

    Expand the fuzzer API to enable more feedback signals to the fuzzer
    This includes the following methods as provided by jazzer:
    - guideTowardsEquality
    - guideTowardsContainment
    - exploreState

--- packages/fuzzer/fuzzer.ts
+++ packages/fuzzer/fuzzer.ts
@@ -28,6 +28,8 @@ addon.registerNewCounters(0, INITIAL_NUM_COUNTERS);
 let currentNumCounters = INITIAL_NUM_COUNTERS;
 let currentCounter = 0;
 
+// Returns the next counter id to use for edge coverage.
+// If needed, the coverage map is enlarged.
 function nextCounter(): number {
 	currentCounter++;
 
@@ -51,6 +53,12 @@ function nextCounter(): number {
 	return currentCounter;
 }
 
+/**
+ * Increments the coverage counter for a given ID.
+ * This function implements the NeverZero policy from AFL++.
+ * See https://aflplus.plus//papers/aflpp-woot2020.pdf
+ * @param id the id of the coverage counter to increment
+ */
 function incrementCounter(id: number) {
 	const counter = coverageMap.readUint8(id);
 	coverageMap.writeUint8(counter == 255 ? 1 : counter + 1, id);
@@ -60,6 +68,16 @@ function readCounter(id: number): number {
 	return coverageMap.readUint8(id);
 }
 
+/**
+ * Performs a string comparison between two strings and calls the corresponding native hook if needed.
+ * This function replaces the original comparison expression and preserves the semantics by returning
+ * the original result after calling the native hook.
+ * @param s1 first compared string
+ * @param s2 second compared string
+ * @param operator the operator used in the comparison
+ * @param id an unique identifier to distinguish between the different comparisons
+ * @returns result of the comparison
+ */
 function traceStrCmp(
 	s1: string,
 	s2: string,
@@ -87,11 +105,21 @@ function traceStrCmp(
 			break;
 	}
 	if (shouldCallLibfuzzer && s1 && s2) {
-		addon.traceUnequalStrings(id, s1, s2);
+		guideTowardsEquality(s1, s2, id);
 	}
 	return result;
 }
 
+/**
+ * Performs an integer comparison between two strings and calls the corresponding native hook if needed.
+ * This function replaces the original comparison expression and preserves the semantics by returning
+ * the original result after calling the native hook.
+ * @param n1 first compared number
+ * @param n2 second compared number
+ * @param operator the operator used in the comparison
+ * @param id an unique identifier to distinguish between the different comparisons
+ * @returns result of the comparison
+ */
 function traceNumberCmp(
 	n1: number,
 	n2: number,
@@ -123,6 +151,63 @@ function traceNumberCmp(
 	}
 }
 
+/**
+ * Instructs the fuzzer to guide its mutations towards making `current` equal to `target`
+ *
+ * If the relation between the raw fuzzer input and the value of `current` is relatively
+ * complex, running the fuzzer with the argument `-use_value_profile=1` may be necessary to
+ * achieve equality.
+ *
+ * @param current a non-constant string observed during fuzz target execution
+ * @param target a string that `current` should become equal to, but currently isn't
+ * @param id a (probabilistically) unique identifier for this particular compare hint
+ */
+export function guideTowardsEquality(
+	current: string,
+	target: string,
+	id: number
+) {
+	addon.traceUnequalStrings(id, current, target);
+}
+
+/**
+ * Instructs the fuzzer to guide its mutations towards making `haystack` contain `needle` as a substring.
+ *
+ * If the relation between the raw fuzzer input and the value of `haystack` is relatively
+ * complex, running the fuzzer with the argument `-use_value_profile=1` may be necessary to
+ * satisfy the substring check.
+ *
+ * @param haystack a non-constant string observed during fuzz target execution
+ * @param needle a string that should be contained in `haystack` as a substring, but
+ *     currently isn't
+ * @param id a (probabilistically) unique identifier for this particular compare hint
+ */
+export function guideTowardsContainment(
+	needle: string,
+	haystack: string,
+	id: number
+) {
+	addon.traceStringContainment(id, needle, haystack);
+}
+
+/**
+ * Instructs the fuzzer to attain as many possible values for the absolute value of `state`
+ * as possible.
+ *
+ * Call this function from a fuzz target or a hook to help the fuzzer track partial progress
+ * (e.g. by passing the length of a common prefix of two lists that should become equal) or
+ * explore different values of state that is not directly related to code coverage.
+ *
+ * Note: This hint only takes effect if the fuzzer is run with the argument
+ * `-use_value_profile=1`.
+ *
+ * @param state a numeric encoding of a state that should be varied by the fuzzer
+ * @param id a (probabilistically) unique identifier for this particular state hint
+ */
+export function exploreState(state: number, id: number) {
+	addon.tracePcIndir(id, state);
+}
+
 // Re-export everything from the native library.
 export type FuzzFn = (data: Uint8Array) => void;
 export type FuzzOpts = string[];
@@ -148,4 +233,16 @@ export const fuzzer: Fuzzer = {
 	readCounter,
 	traceStrCmp,
 	traceNumberCmp,
+};
+
+export interface Jazzer {
+	guideTowardsEquality: typeof guideTowardsEquality;
+	guideTowardsContainment: typeof guideTowardsContainment;
+	exploreState: typeof exploreState;
+}
+
+export const jazzer: Jazzer = {
+	guideTowardsEquality,
+	guideTowardsContainment,
+	exploreState,
 };
