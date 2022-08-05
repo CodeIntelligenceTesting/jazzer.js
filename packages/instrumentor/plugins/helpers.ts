04cc78303511710b020eb9d2f5c91d4755debcaf
Khaled Yakdan <yakdan@code-intelligence.com>
2022-08-05 14:50:18 +0200

    Instrument switch statements
    This traces the comparisons between the switch variable and checks
    taking place in the cases

--- packages/instrumentor/plugins/helpers.ts
+++ packages/instrumentor/plugins/helpers.ts
@@ -1,7 +1,11 @@
-import { NumericLiteral } from "@babel/types";
+import { NumericLiteral, Identifier } from "@babel/types";
 import { types } from "@babel/core";
 import * as crypto from "crypto";
 
 export function fakePC(): NumericLiteral {
 	return types.numericLiteral(crypto.randomInt(512));
+}
+
+export function newIdentifier(prefix: string): Identifier {
+	return types.identifier(`jazzer_${prefix}_${crypto.randomInt(512)}`);
 }
