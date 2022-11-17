308930370ea022b02a1106b3f70f865e90eb8bef
Khaled Yakdan <yakdan@code-intelligence.com>
2022-11-17 05:37:34 +0100

    Add an example for jest integration

---
+++ examples/jest_integration/foo.js
@@ -0,0 +1,13 @@
+module.exports.fuzzMe = function (data) {
+	const s = data.toString();
+	if (s.length !== 16) {
+		return;
+	}
+	if (
+		s.slice(0, 8) === "Awesome " &&
+		s.slice(8, 15) === "Fuzzing" &&
+		s[15] === "!"
+	) {
+		throw Error("Welcome to Awesome Fuzzing!");
+	}
+};
