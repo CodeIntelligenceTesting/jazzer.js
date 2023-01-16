7e25291b79c0fe394bee37f536187562cb63fd09
Khaled Yakdan <yakdan@code-intelligence.com>
2023-01-16 05:51:21 +0100

    instrumentor: remove unused function

--- packages/instrumentor/plugins/helpers.ts
+++ packages/instrumentor/plugins/helpers.ts
@@ -1,11 +1,7 @@
-import { NumericLiteral, Identifier } from "@babel/types";
+import { NumericLiteral } from "@babel/types";
 import { types } from "@babel/core";
 import * as crypto from "crypto";
 
 export function fakePC(): NumericLiteral {
 	return types.numericLiteral(crypto.randomInt(512));
-}
-
-export function newIdentifier(prefix: string): Identifier {
-	return types.identifier(`jazzer_${prefix}_${crypto.randomInt(512)}`);
 }
