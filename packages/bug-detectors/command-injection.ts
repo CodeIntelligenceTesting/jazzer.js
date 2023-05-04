119d8bef753db4ff6d1dd23e358e3e337863565a
Peter Samarin <peter.samarin@code-intelligence.com>
2023-05-04 15:23:35 +0200

    command injection: simplify implementation
    - Also defer solving the problem of hooking functions used by
    Jazzer.js internally into the future

--- packages/bug-detectors/command-injection.ts
+++ packages/bug-detectors/command-injection.ts
@@ -17,96 +17,72 @@
 import {
 	BugDetectorError,
 	hookBuiltInFunction,
-	OriginalFnInfo,
 	saveFirstBugDetectorError,
 } from "./index";
 import { nextFakePC } from "@jazzer.js/instrumentor/dist/plugins/helpers";
 import { guideTowardsEquality } from "@jazzer.js/fuzzer";
 
-export const commandInjectionEvilCommand =
-	process.platform === "win32" ? "copy NUL EVIL" : "touch EVIL";
-
-export const commandInjectionFnsToHook = [
-	{ moduleName: "child_process", functionName: "exec" },
-	{ moduleName: "child_process", functionName: "execSync" },
-	{ moduleName: "child_process", functionName: "execFile" },
-	{ moduleName: "child_process", functionName: "execFileSync" },
-	{ moduleName: "child_process", functionName: "spawn" },
-	{ moduleName: "child_process", functionName: "spawnSync" },
-	{ moduleName: "child_process", functionName: "fork" },
-];
-
 // eslint-disable-next-line @typescript-eslint/ban-types
-export async function registerCommandInjectionBugDetector<F extends Function>(
+export async function registerCommandInjectionBugDetectors<F extends Function>(
 	callOriginalFn: boolean
-): Promise<OriginalFnInfo[]> {
-	return await registerBugDetector<F>(
-		commandInjectionEvilCommand,
-		"Command Injection: ",
-		callOriginalFn,
-		commandInjectionFnsToHook
-	);
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-export async function registerBugDetector<F extends Function>(
-	evilCommand: string,
-	errorString: string,
-	callOriginalFn: boolean,
-	targetFunctions: { moduleName: string; functionName: string }[]
-): Promise<OriginalFnInfo[]> {
-	// eslint-disable-next-line @typescript-eslint/ban-types
-	async function registerBugDetectorInternal(
+): Promise<void> {
+	async function registerBugDetector(
 		moduleName: string,
-		targetFnName: string,
+		targetFunctionName: string,
+		evilCommand: string,
+		baseErrorString: string,
 		callOriginalFn: boolean
-	): Promise<OriginalFnInfo> {
+	): Promise<F> {
 		const id = nextFakePC();
-
-		return [
+		return await hookBuiltInFunction(
 			moduleName,
-			targetFnName,
-			await hookBuiltInFunction(
-				moduleName,
-				targetFnName,
-				(
-					originalFn: F,
-					cmdOrFileOrPath: string,
-					...args: unknown[]
-				): F | void => {
-					guideTowardsEquality(cmdOrFileOrPath, evilCommand, id);
-					if (cmdOrFileOrPath.includes(evilCommand)) {
-						const err = new BugDetectorError(
-							errorString +
-								targetFnName +
-								"() called with command: '" +
-								cmdOrFileOrPath +
-								"'"
-						);
-
-						// Remove the first 3 lines after the message from the stack trace.
-						// The first lines are internal Jazzer function calls.
-						saveFirstBugDetectorError(err, 3);
-						throw err;
-					}
-					if (callOriginalFn) {
-						return originalFn(cmdOrFileOrPath, ...args);
-					}
+			targetFunctionName,
+			(
+				originalFn: F,
+				cmdOrFileOrPath: string,
+				...args: unknown[]
+			): F | void => {
+				guideTowardsEquality(cmdOrFileOrPath, evilCommand, id);
+				if (cmdOrFileOrPath.includes(evilCommand)) {
+					const err = new BugDetectorError(
+						baseErrorString +
+							targetFunctionName +
+							"() called with command: '" +
+							cmdOrFileOrPath +
+							"'"
+					);
+					// Remove the first 3 lines after the message from the stack trace.
+					// The first lines are internal Jazzer function calls.
+					saveFirstBugDetectorError(err, 3);
+					throw err;
 				}
-			),
-		];
-	}
-
-	const functionInfo: OriginalFnInfo[] = [];
-	for (const targetFunction of targetFunctions) {
-		functionInfo.push(
-			await registerBugDetectorInternal(
-				targetFunction.moduleName,
-				targetFunction.functionName,
-				callOriginalFn
-			)
+				if (callOriginalFn) {
+					return originalFn(cmdOrFileOrPath, ...args);
+				}
+			}
 		);
 	}
+	const evilCommand =
+		process.platform === "win32" ? "copy NUL EVIL" : "touch EVIL";
+	const baseErrorString = "Command Injection: ";
+	const moduleName = "child_process";
+	const functionNames = [
+		"exec",
+		"execSync",
+		"execFile",
+		"execFileSync",
+		"spawn",
+		"spawnSync",
+		"fork",
+	];
 
-	return functionInfo;
+	for (const targetFunction of functionNames) {
+		await registerBugDetector(
+			moduleName,
+			targetFunction,
+			evilCommand,
+			baseErrorString,
+			callOriginalFn
+		);
+	}
 }
