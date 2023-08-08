5d83cbd1ba1eca451e4a034aeb629f6b5c1762e0
Norbert Schneider <norbert.schneider@code-intelligence.com>
2023-08-08 11:31:11 +0200

    jest-runner: Always instrument in fuzzing mode

---
+++ tests/jest_integration/jest_project/target.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Code Intelligence GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const fuzzMe = function (data) {
+	if (data.toString() === "Awesome") {
+		throw Error("Welcome to Awesome Fuzzing!");
+	}
+};
+
+const asyncFuzzMe = function (data) {
+	return new Promise((resolve, reject) => {
+		try {
+			fuzzMe(data);
+			resolve();
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+
+const callbackFuzzMe = function (data, done) {
+	setImmediate(() => {
+		try {
+			fuzzMe(data);
+			done();
+		} catch (e) {
+			done(e);
+		}
+	});
+};
+
+module.exports.fuzzMe = fuzzMe;
+module.exports.asyncFuzzMe = asyncFuzzMe;
+module.exports.callbackFuzzMe = callbackFuzzMe;
