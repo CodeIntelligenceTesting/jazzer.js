544c7ebf2b2df3e981ba8b836e7188d14ea99622
Brian Lewis <keller@csh.rit.edu>
2023-10-23 14:25:17 +0200

    Time to debug in CI

--- end-to-end/package-jazzer-js.sh
+++ end-to-end/package-jazzer-js.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/opt/homebrew/bin/bash
 set -e
 
 main() {
@@ -8,14 +8,20 @@ main() {
     npm run build --workspace='@jazzer.js/fuzzer'
     npm run prepack --workspace='@jazzer.js/fuzzer'
 
-    npm pack --workspaces | sed_version_and_mv 
+    local tarballs=$(npm pack --workspaces)
+    echo "$tarballs"
+    echo "$tarballs" | sed_version_and_mv 
 }
 
 sed_version_and_mv() {
     while read data; do
-        local no_version=$(echo $data | sed -r -f end-to-end/remove-version.sed)
-        echo "mv $data end-to-end/$no_version"
-        mv $data end-to-end/$no_version
+        if [[ -n "$data" ]]; then
+            local no_version=$(echo $data | sed -r -f end-to-end/remove-version.sed)
+            echo "mv $data end-to-end/$no_version"
+            mv $data end-to-end/$no_version
+        else
+            echo "received empty line"
+        fi
     done
 }
 
