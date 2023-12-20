1c06abecb61881d93fd9eec3869ff4f9533679fc
Peter Samarin <peter.samarin@code-intelligence.com>
2023-12-20 20:35:49 +0100

    tests: linter shouldn't add license header to coverage tests

--- tests/code_coverage/sample_fuzz_test/codeCoverage.fuzz.js
+++ tests/code_coverage/sample_fuzz_test/codeCoverage.fuzz.js
@@ -1,10 +1,4 @@
-/*
- * Copyright 2023 Code Intelligence GmbH
- *
- * Unless required by applicable law or agreed to in writing, this software
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
- * ANY KIND, either express or implied.
- */
+/* eslint-disable header/header */
 
 const target = require("./fuzz.js");
 
